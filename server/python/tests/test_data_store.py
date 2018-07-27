3920eea61aa5d65b53fcffe85b977c4c933def31
Nick Barlow <nbarlow@turing.ac.uk>
2018-07-27 13:34:28 +0100

    skip some python tests when running without data-store

--- server/python/tests/test_data_store.py
+++ server/python/tests/test_data_store.py
@@ -2,13 +2,20 @@
 Check we can read and write to the data store.
 """
 
+import os
+import pytest
+
 from python_service import read_frame, write_frame, retrieve_frames
 
 frame_hash = 'abc123def'
 frame_name = 'testframe'
-datastore_base_url = 'http://localhost:7102'
+if "DATASTORE_URI" in os.environ.keys():
+    datastore_base_url = os.environ["DATASTORE_URI"]
+else:
+    datastore_base_url = 'http://localhost:7102'
 
-
+@pytest.mark.skipif("WRATTLER_LOCAL_TEST" in os.environ.keys(),
+                    reason="Needs data-store to be running")
 def test_write_frame():
     """
     Write a frame
@@ -22,7 +29,8 @@ def test_write_frame():
                            frame_hash)
     assert(wrote_ok==True)
 
-
+@pytest.mark.skipif("WRATTLER_LOCAL_TEST" in os.environ.keys(),
+                    reason="Needs data-store to be running")
 def test_read_frame():
     """
     Read back the same frame
@@ -34,7 +42,8 @@ def test_read_frame():
     assert(data[1]["var_1"]=="456")
     assert(data[1]["var_2"]=="def")
 
-
+@pytest.mark.skipif("WRATTLER_LOCAL_TEST" in os.environ.keys(),
+                    reason="Needs data-store to be running")
 def test_retrieve_frames():
     """
     as used by eval function - get frames from url,
@@ -46,4 +55,3 @@ def test_retrieve_frames():
                                              frame_name)}]
     data = retrieve_frames(frame_list)
     print(data)
-
