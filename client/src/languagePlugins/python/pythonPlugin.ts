71b67d07c124c1e73fb539bd4193a38a0612c1fb
myyong <myong@turing.ac.uk>
2018-10-09 15:04:23 +0100

    Add preview to python plugin

--- client/src/languagePlugins/python/pythonPlugin.ts
+++ client/src/languagePlugins/python/pythonPlugin.ts
@@ -82,15 +82,64 @@ class PythonBlockKind implements Langs.Block {
                                       .concat("\n");
 				}
 				return valuesString
-      }
+			}
+			
+			function printCurrentTables(cellValues) {
+				let tableNames:Array<string> = Object.keys(cellValues)
+				let tablesComponent: Array<any> = [];
+				// for each variable, print a table
+				for (let v = 0; v < tableNames.length; v++) {
+					let aTable = cellValues[tableNames[v]].data;
+					// get keys of each dataframe variable
+					let tableHeaders:Array<string> = getCurrentHeaders(aTable[0]);
+					
+					let rowsComponents:Array<any> = []
+					let headerComponents:Array<any> = []
+					// for this table, create headers
+					for (let i = 0; i < tableHeaders.length; i++) {
+						headerComponents.push(h('th',{}, [tableHeaders[i]]))
+					}
+					rowsComponents.push(h('tr',{},[headerComponents]))
+					
+					// for every row in dataframe, create rows
+					for (let row = 0; row < aTable.length; row++) {
+						let values = getCurrentRow(aTable[row], tableHeaders);
+						let columnsComponents:Array<any> = []
+						for (let v = 0; v < values.length; v++) {
+							columnsComponents.push(h('td', {}, [values[v].toString()]))
+						}
+						rowsComponents.push(h('tr',{},[columnsComponents]))
+					}
 
-      let previewButton = h('button', { onclick:() => context.evaluate(cell) }, ["Preview"])
-      let preview = h('div', {}, [
-        h('p', {
-            style: "height:75px; position:relative", 
-          }, 
-          [((cell.code==undefined)||(cell.code.value==undefined)) ? previewButton : (printPreview(cell.code.antecedents))]),
-      ]);
+					let tableComponent = h('table', {style: "width:100%"},[rowsComponents]);
+					tablesComponent.push(tableComponent)
+        }
+				return h('div', {},[tablesComponent]);
+			}
+			
+			function getCurrentHeaders(firstDataFrameRow) {
+				let tableHeaders:Array<string> = Object.keys(firstDataFrameRow)
+				return tableHeaders;
+			}
+
+			function getCurrentRow(dfRow, keys) {
+				let row: Array<string> = [];
+				for (let k in keys) {
+					row.push(dfRow[keys[k]]);
+				} 
+				return row;
+			}
+
+      // let previewButton = h('button', { onclick:() => context.evaluate(cell) }, ["Preview"])
+      // let preview = h('div', {}, [
+      //   h('p', {
+      //       style: "height:75px; position:relative", 
+      //     }, 
+      //     [((cell.code==undefined)||(cell.code.value==undefined)) ? previewButton : (printPreview(cell.code.antecedents))]),
+			// ]);
+			
+			let previewButton = h('button', { onclick:() => context.evaluate(cell) }, ["Preview"])
+      let preview = h('div', {}, [((cell.code==undefined)||(cell.code.value==undefined)) ? previewButton : (printCurrentTables(cell.code.value))]);
 
       let evalButton = h('button', { onclick:() => {context.evaluate(cell); console.log(cell)} }, ["Evaluate"])
       let results = h('div', {}, [
@@ -98,7 +147,6 @@ class PythonBlockKind implements Langs.Block {
             style: "height:75px; position:relative", 
           }, 
           [ ((cell.code==undefined)||(cell.code.value==undefined)) ? evalButton : (printValue(cell.code.value))]),
-          // [ ((cell.code==undefined)||(cell.code.value==undefined)) ? evalButton : ("Value is: " + JSON.stringify(cell.code.value.data)) ]),
       ]);
 
  
