b42f80557640f7205023db14768d3897a98a581b
myyong <myong@turing.ac.uk>
2019-01-29 16:33:29 +0000

    XKCD in place of Wrattler

---
+++ client/exports/editors/preview.d.ts
@@ -0,0 +1,4 @@
+import { VNode } from 'maquette';
+import * as Values from '../definitions/values';
+declare function printPreview(cellId: number, triggerSelect: (number) => void, selectedTable: number, cellValues: Values.ExportsValue): VNode;
+export { printPreview };
