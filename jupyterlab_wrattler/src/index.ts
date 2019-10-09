25bb4095c25e8ce570025ee5697eda1649e774b2
myyong <myong@turing.ac.uk>
2019-10-09 11:22:28 +0100

    Flag use docker paths

--- jupyterlab_wrattler/src/index.ts
+++ jupyterlab_wrattler/src/index.ts
@@ -14,7 +14,7 @@ const CSS_CLASS = 'jp-Wrattler';
  */
 export const MIME_TYPE = 'text/plain';
 /** @hidden */
-export const USE_BINDER= true
+export const USE_BINDER= false
 
 class RenderedWrattler extends Widget implements IRenderMime.IRenderer {
 
