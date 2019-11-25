32d2bc81a4f1a05ffddfa9747491e99dd5b2d4da
myyong <myong@turing.ac.uk>
2019-11-25 14:54:05 +0000

    Fix spinner icon for javascript cell

--- client/src/languages/javascript.ts
+++ client/src/languages/javascript.ts
@@ -205,7 +205,7 @@ export const javascriptEditor : Langs.Editor<JavascriptState, JavascriptEvent> =
       { class:'preview-button', onclick:() => { 
           Log.trace("editor", "Evaluate button clicked in external language plugin")
           context.evaluate(cell.editor.id) } }, ["Evaluate!"] )
-    let spinner = h('i', {id:'cellSpinner_'+cell.editor.id, class: 'fas fa-spinner fa-spin' }, [])
+    let spinner = h('i', {id:'cellSpinner_'+cell.editor.id, class: 'fa fa-spinner fa-spin' }, [])
     let triggerSelect = (t:number) => context.trigger({kind:'switchtab', index: t})
     let preview = h('div', {class:'preview'}, [(cell.code.value==undefined) ? (cell.evaluationState=='pending')?spinner:previewButton : (createOutputPreview(cell, triggerSelect, state.tabID, <Values.ExportsValue>cell.code.value))]);
     let code = createMonacoEditor("javascript", state.block.source, cell, context)
