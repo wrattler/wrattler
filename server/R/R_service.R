0ce5383ea119ac2abe1564d2bf9f271e51754bf6
Nick Barlow <nbarlow@turing.ac.uk>
2019-11-25 11:05:40 +0000

    more protection against NULL dataframes

--- server/R/R_service.R
+++ server/R/R_service.R
@@ -92,13 +92,17 @@ getNameAndHashFromURL <- function(url) {
 
 getFileContent <- function(url) {
     ## retrieve contents of a file (e.g. containing function definitions) from the datastore
-    r <- tryCatch({ GET(url, add_headers(Accept="text/html")) },
-       error=function(cond) {
-           filenameAndHash <- getNameAndHashFromURL(url)
-           return(GET(makeURL(filenameAndHash[[1]], filenameAndHash[[2]]),
-                      add_headers(Accept="text/html")))
-       }
-    )
+    r <- tryCatch({
+        ## first try to construct the URL ourselves based on
+        ## the hash, filename, and our datastore URL
+        filenameAndHash <- getNameAndHashFromURL(url)
+        return(GET(makeURL(filenameAndHash[[1]], filenameAndHash[[2]]),
+                   add_headers(Accept="text/html")))
+    }, error=function(cond) {
+            ## try falling back on the URL we were given
+            GET(url, add_headers(Accept="text/html"))
+    })
+
     if ( r$status != 200) {
         print("Unable to access datastore")
         return(NULL)
@@ -219,6 +223,8 @@ jsonFromImageFile <- function(frameName, cellHash) {
 
 
 jsonFromDataFrame <- function(frameData) {
+    if (is.null(frameData)) return(NULL)
+
     ## jsonlite doesn't like converting numbers into json
     if (is.numeric(frameData)) frameData <- as.character(frameData)
     ## explicitly convert into a dataframe if we can
@@ -226,7 +232,6 @@ jsonFromDataFrame <- function(frameData) {
                           error=function(cond) {
                               return(NULL)
                           })
-    if (is.null(frameData)) return(NULL)
     ## convert to JSON
     frameJSON <- jsonlite::toJSON(frameData, na="null")
     return(frameJSON)
